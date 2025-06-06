922ded7fe38e219d582dc3ca4f43d7777593eae6
Cameron Stanley <cam96stanley@gmail.com>
2025-06-05 20:55:54 -0400

    updated diagnosis route to create better response for create diagnonsis

--- sql/blueprints/diagnosis/schemas.py
+++ sql/blueprints/diagnosis/schemas.py
@@ -1,18 +1,24 @@
 from marshmallow_sqlalchemy import auto_field
+from marshmallow import fields
 from sql.extensions import ma
 from sql.models import Diagnosis
 from sql.blueprints.user.schemas import UserSchema
 
-class DiagnosisSchema(ma.SQLAlchemyAutoSchema):
-  patient = ma.Nested(UserSchema)
-  
-  patient_id = ma.auto_field(dump_only=True)
-  doctor_id = ma.auto_field(dump_only=True)
-  created_at = auto_field(dump_only=True)
+class DiagnosisSchema(ma.SQLAlchemySchema):
   class Meta:
     model = Diagnosis
     load_instance = True
     include_fk = True
+    
+  patient = fields.Nested(UserSchema(only=("id", "name")))
+  doctor = fields.Nested(UserSchema(only=("id", "name")))
+  
+  created_at = auto_field(dump_only=True)
+  diagnosis_name = auto_field()
+  diagnosis_code = auto_field()
+  diagnosis_date = auto_field()
+  notes = auto_field()
+  id = auto_field()
 
 diagnosis_schema = DiagnosisSchema()
 diagnoses_schema = DiagnosisSchema(many=True)
